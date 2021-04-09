700887fe982321faafa19bdad10f1232fe2f6014
Abdisamad Hussein <78183395+abdi-hu@users.noreply.github.com>
2021-04-09 17:17:21 -0400

    sort index

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,12 +1,15 @@
 const Job = require("../models/job");
 
 async function index(req, res) {
-	const jobs = await Job.find({});
+	const jobs = await Job.find({}).sort("companyName");
 	res.status(200).json(jobs);
 }
 async function create(req, res) {
-	const job = await Job.create(req.body);
-	res.status(201).json(job);
+	try {
+		console.log(req.body);
+		const job = await Job.create(req.body);
+		res.status(201).json(job);
+	} catch (error) {}
 }
 module.exports = {
 	index,
